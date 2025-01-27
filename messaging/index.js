68726a48e11c6507e95f928585967f4ceea1d687
lenaqian <135063526+lenaqian@users.noreply.github.com>
2025-01-27 14:14:22 -0500

    scale horizontally basic messaging

--- messaging/index.js
+++ messaging/index.js
@@ -2,35 +2,96 @@ const express = require('express');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+//for scaling horizontally
+const { availableParallelism } = require('node:os');
+const cluster = require('node:cluster');
+const { createAdapter, setupPrimary } = require('@socket.io/cluster-adapter');
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server);
-
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
-});
-
-io.on('connection', (socket) => {
-    console.log('A user connected');
-
-    // Emit 'hello', 'world' to the connected client on connection!
-    io.emit('hello', 'WELCOME TO DA CHAT');
-
-    // Listen for 'chat message' from the client
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg); // Broadcast the message to all clients
+if (cluster.isPrimary) {
+  const numCPUs = availableParallelism();
+  // create one worker per available core
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork({
+      PORT: 3000 + i
     });
+  }
+  
+  // set up the adapter on the primary thread
+  return setupPrimary();
+}
 
-    // Handle disconnection
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
+async function main() {
+  // open the database file
+  const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+  });
+
+  // create our 'messages' table (you can ignore the 'client_offset' column for now)
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+  const app = express();
+  const server = createServer(app);
+  const io = new Server(server, {
+    connectionStateRecovery: {}, 
+    // set up the adapter on each worker thread
+    adapter: createAdapter()
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
+
+  io.on('connection', async (socket) => {
+    socket.on('chat message', async (msg, clientOffset, callback) => {
+      let result;
+      try {
+        // store the message in the database
+        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+      } catch (e) {
+        if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+          // the message was already inserted, so we notify the client
+          callback();
+        } else {
+          // nothing to do, just let the client retry
+        }
+        return;
+      }
+      // include the offset with the message
+      io.emit('chat message', msg, result.lastID);
+          // acknowledge the event
+      callback();
     });
-});
+    if (!socket.recovered) {
+      // if the connection state recovery was not successful
+      try {
+        await db.each('SELECT id, content FROM messages WHERE id > ?',
+          [socket.handshake.auth.serverOffset || 0],
+          (_err, row) => {
+            socket.emit('chat message', row.content, row.id);
+          }
+        )
+      } catch (e) {
+        // something went wrong
+      }
+    }
+  });
+ 
 
+  // each worker will listen on a distinct port
+  const port = process.env.PORT;
 
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+main();
