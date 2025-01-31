e1a926abb00ee8803fe9a8be453fdd80ae778d42
niksaitov <nik.saitov.04@gmail.com>
2025-01-30 21:35:57 -0500

    feat: connect prisma db, implement test query, delete unused files

--- nextjs-prototype/app/components/ride-card.tsx
+++ nextjs-prototype/app/components/ride-card.tsx
@@ -1,12 +1,14 @@
 import type { Ride } from '../types/main'
 
-export default function RideCard({ ride }: { ride: Ride })
+export default function RideCard(ride : Ride)
 {
+  const convertedMonth:number = (ride.dateTime.getMonth() + 1) % 13;
+
   return (
-    <div className="rounded-lg border p-4 text-center">
+    <div className='rounded-lg border p-4 text-center'>
       <h2>Route: {ride.beginning} - {ride.destination}</h2>
-      <p>Date: {ride.date}</p>
-      <p>Start time: {ride.startTime}</p>
+      <p>Date: {convertedMonth}/{ride.dateTime.getDate()}/{ride.dateTime.getFullYear()}</p>
+      <p>Start time: {ride.dateTime.getHours()}:{ride.dateTime.getMinutes()}</p>
     </div>
   );
 }
---
+++ nextjs-prototype/app/components/search-bar.tsx
@@ -0,0 +1,8 @@
+export default function SearchBar()
+{
+  return (
+    <div className='text-center'>
+      <input className='border rounded-lg p-4 m-4 w-96 bg-black placeholder-white' type='text' placeholder='Search for a ride' />
+    </div>
+  );
+}
