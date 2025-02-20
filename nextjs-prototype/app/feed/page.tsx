1db0068aefb3d1cc0b0dfd5b5e1fda44bdc07516
niksaitov <nik.saitov.04@gmail.com>
2025-02-20 17:58:47 -0500

    feat: db changes

--- nextjs-prototype/app/feed/page.tsx
+++ nextjs-prototype/app/feed/page.tsx
@@ -13,13 +13,14 @@ import { TopBar } from "@/components/top-bar"
 import Link from "next/link"
 
 export default async function Home() {
-  // Example: fetch 5 rides from the DB, sorting by recency.
-  // const fetchedRides = await prisma.ride.findMany({
-  //   take: 5,
-  //   orderBy: { dateTime: "desc" },
-  // })
-
-  const fetchedRides = []
+  const fetchedRides = await prisma.ride.findMany({
+    take: 5,
+    orderBy: { startTime: "desc" },
+    where: { 
+      isClosed: false,
+      startTime: { gte: new Date() },
+    }
+  })
 
   return (
     <SidebarProvider>
