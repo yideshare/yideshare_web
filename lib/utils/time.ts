c244f182e0932a6b0608acdaa5099bfbe629a344
niksaitov <nik.saitov.04@gmail.com>
2025-04-09 13:51:16 -0400

    chore: cleanup debug print statements

--- lib/utils/time.ts
+++ lib/utils/time.ts
@@ -3,16 +3,10 @@ export function createStartEndDateTimes(
   startTime: string,
   endTime: string
 ) {
-  
-  console.log("FIRST INPUT")
-  console.log(startTime, endTime)
+
   // split startTime and endTime strings into hours and minutes
   const [startHours, startMinutes] = startTime.split(":").map(Number);
   const [endHours, endMinutes] = endTime.split(":").map(Number);
-
-  console.log("INPUT")
-  console.log(startHours, startMinutes)
-  console.log(endHours, endMinutes)
 
   // create valid Date objects by combining the selected date with the start and end times
   const startDate = new Date(date);
@@ -21,10 +15,6 @@ export function createStartEndDateTimes(
   // set the correct hours and minutes for start and end times
   startDate.setHours(startHours, startMinutes, 0, 0);
   endDate.setHours(endHours, endMinutes, 0, 0);
-
-  console.log("RETURNING")
-  console.log(startDate)
-  console.log(endDate)
 
   return {
     startTimeObject: startDate,
