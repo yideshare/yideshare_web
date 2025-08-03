8c08810fc2755e411b39e69b0dc2bb6411accc62
niksaitov <nik.saitov.04@gmail.com>
2025-08-03 17:06:28 -0400

    fix: require eslint for vercel, change lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,10 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     // rules: {
-    //   "@typescript-eslint/no-explicit-any": "off",
+    //
     // },
   },
 ];
-
 
 export default eslintConfig;
