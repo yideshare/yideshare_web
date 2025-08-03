f481c900736e992cba93be82c282e7edbb57a451
niksaitov <nik.saitov.04@gmail.com>
2025-08-03 16:46:59 -0400

    feat: change db provider to postgresql

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,8 +4,16 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const prismaInstance = globalForPrisma.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+  globalForPrisma.prisma = prismaInstance;
 }
+
+export { prismaInstance as prisma };
