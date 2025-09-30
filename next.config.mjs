d89ed7050060c245da7baa9f210d6bf3a009d486
niksaitov <nik.saitov.04@gmail.com>
2025-09-30 16:55:03 -0400

    fix: rm comment in config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable checks to get the build working
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
-  
-  // Production settings from your CJS file
+
   output: 'standalone',
   env: {
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE || '',
